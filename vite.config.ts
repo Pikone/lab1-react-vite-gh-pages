824f5e1985f7a7ae04f6f41242be914349f8c946
pikone <pikone@ya.ru>
2025-11-26 21:13:54 +0000

    fix: split vite and vitest configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/lab1-react-vite-gh-pages',
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-    css: true,
-  },
 });
